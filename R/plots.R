2a44fa742d2f74a7717b08fae8c59066901990d5
Xavier Farré Manzorro <xaviescacs@gmail.com>
2026-08-06 17:06:48 +0200

    Enhance plot with better alpha and colors. Segment indicating mean.

--- R/plots.R
+++ R/plots.R
@@ -48,17 +48,25 @@ theme_dist_oneline <- function(...) {
 #' 
 plot_dist_oneline <- function(x,limits = NULL){
   den <- density(x)
+  color_blue <- get_csdta_colours("bright_blue")
+  color_red <- get_csdta_colours("red")
+  color_dark <- get_csdta_colours("dark")
+  color_sea <- get_csdta_colours("sea")
+  x_mean <- mean(x)
   data.frame(x = x,density = approx(den$x, den$y, xout = x)$y) |> 
     dplyr::mutate(density_scaled = (density)*50) |> 
     ggplot2::ggplot(ggplot2::aes(x = x, y = 0)) +  
       ggdist::stat_halfeye(
+        fill = color_sea,
         adjust = .5, 
         width = .6, 
         .width = 0, 
         justification = -.5, 
         point_colour = NA) + 
-      ggplot2::geom_boxplot(outlier.shape = NA,box.colour = get_csdta_colours("bright_blue")) +
-      ggplot2::geom_point(ggplot2::aes(size = density_scaled,alpha = density**2),color = get_csdta_colours("bright_blue")) +
+      ggplot2::geom_boxplot(outlier.shape = NA,box.color = ,whisker.color = color_dark) +
+      ggplot2::geom_point(ggplot2::aes(size = density),alpha = 0.1,color = color_blue) +
+      #ggplot2::geom_vline(ggplot2::aes(xintercept = x_mean), color = color_red, size = 0.5) +
+      ggplot2::geom_segment(x = x_mean, xend = x_mean, y = 0, yend = 1, color = color_red,linewidth = 0.5) +
       ggplot2::scale_x_continuous(limits = limits) +
       theme_dist_oneline()
   
