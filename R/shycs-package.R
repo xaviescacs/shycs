96ae1e1c17f2f6f39b5ec83e452abe90e9f7a429
Xavier Farré Manzorro <xaviescacs@gmail.com>
2026-08-12 23:37:39 +0200

    Update package files.

---
+++ R/shycs-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom shiny is.reactive
+#' @importFrom shiny moduleServer
+#' @importFrom shiny NS
+#' @importFrom shiny plotOutput
+#' @importFrom shiny renderPlot
+#' @importFrom shiny tag
+## usethis namespace: end
+NULL
