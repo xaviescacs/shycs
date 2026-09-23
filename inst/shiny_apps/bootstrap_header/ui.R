f5ed66936b90998715c5985837812f08980b4af3
Xavier Farré Manzorro <xaviescacs@gmail.com>
2026-09-23 13:22:03 +0200

    Add app to display the csdata header.

---
+++ inst/shiny_apps/bootstrap_header/ui.R
@@ -0,0 +1,11 @@
+htmltools::tagList(
+  bslib::bs_theme_dependencies(csdta_bs_theme()),
+  ui_csdta_header(languages = c("ca","es","en"),title = "This is a reasonable long title for a shiny app."),
+  htmltools::div(class = "container",
+    selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+    verbatimTextOutput("summary"),
+    tableOutput("table")
+  )
+
+)
+
