96d7e39c3101079cdb354e4f19426aa145511351
Xavier Farré Manzorro <xaviescacs@gmail.com>
2026-09-24 15:57:21 +0200

    Add theory modal with fake text.

--- R/ui_header.R
+++ R/ui_header.R
@@ -26,7 +26,53 @@ ui_csdta_header <- function(title,theory=TRUE,credits=TRUE,languages,csdta_link=
     languages_li_tags <- ""
   }
 
+  modal_body_taglist <- htmltools::tagList(
+    p("Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Pellentesque vitae ipsum et ipsum imperdiet placerat. 
+            Mauris sodales ac nibh vitae eleifend. Suspendisse fermentum 
+            dolor eget ante lobortis, eu dictum felis venenatis. 
+            Aenean fringilla vehicula dui vel sollicitudin. Praesent 
+            facilisis, nisi et ullamcorper facilisis, ex ex blandit velit, 
+            a ullamcorper est lacus finibus elit. Nunc a urna nec sem 
+            placerat congue non et diam. In rutrum suscipit convallis. 
+            Nam sit amet neque non lorem feugiat mollis sed id lorem. 
+            Maecenas tristique rhoncus lectus, eget aliquet massa fermentum 
+            nec."),
+            p("Vivamus gravida elementum diam. Maecenas a luctus neque. 
+            Integer molestie nibh tellus, id tempus diam convallis et. 
+            Curabitur tempus sem mauris, non interdum dui blandit sit amet. 
+            Nunc condimentum diam eget venenatis fringilla. Ut quis enim 
+            venenatis lacus feugiat volutpat id at purus. Nullam iaculis 
+            diam id viverra dignissim."),
+            p("Etiam nec aliquam neque. Morbi metus ipsum, molestie tincidunt 
+            cursus at, consequat ut elit. Etiam egestas consectetur tempus. 
+            In tincidunt, ligula ut bibendum lacinia, purus ligula gravida 
+            urna, vitae pretium ex augue vitae elit. Sed faucibus a tellus 
+            a volutpat. Ut consectetur, nisl in elementum iaculis, tellus 
+            arcu imperdiet turpis, vel fringilla tortor felis ut dui. 
+            Integer sit amet semper felis, non venenatis urna."),
+            p("Quisque porta mauris sit amet tortor cursus faucibus. Suspendisse 
+            velit tortor, dignissim ac odio ac, dapibus fermentum quam. 
+            Maecenas nulla est, sollicitudin in vehicula sit amet, pellentesque 
+            sed ex. Nam felis urna, mattis et faucibus nec, malesuada tempus 
+            libero. Vivamus nec massa nibh. Donec eget ligula sapien. 
+            Pellentesque ornare lectus at urna consectetur venenatis."),
+            p("Vivamus sed rutrum metus. Curabitur sit amet est tincidunt 
+            elit pellentesque placerat. Proin non erat mi. Maecenas porttitor 
+            pretium purus, quis dictum justo aliquam vitae. Nunc eros tortor, 
+            sollicitudin quis nisi in, mollis commodo purus. Integer a est elit. 
+            Vivamus a pellentesque turpis. Maecenas id metus id felis mattis 
+            commodo posuere vel lacus. Aliquam eleifend bibendum magna, 
+            aliquet tincidunt diam imperdiet eu. In congue nibh nisl, 
+            sollicitudin congue ex molestie quis. Morbi non nibh mollis 
+            turpis posuere accumsan ac sit amet sem. Nunc vestibulum urna 
+            ac magna porta porttitor.")
+  )
+
+  modal_body_taglist <- modal_body_taglist |> c(modal_body_taglist)
+
   htmltools::withTags(
+    tagList(
     header(
       class = "csdta-header p-0 text-bg-dark",
       div(
@@ -58,6 +104,8 @@ ui_csdta_header <- function(title,theory=TRUE,credits=TRUE,languages,csdta_link=
                 button(
                   id = "button-theory",
                   class = "btn btn-primary",
+                  `data-bs-toggle`="modal",
+                  `data-bs-target`="#theory-modal",
                   "Theory"
                 )
               },
@@ -65,6 +113,8 @@ ui_csdta_header <- function(title,theory=TRUE,credits=TRUE,languages,csdta_link=
                 button(
                   id = "button-credits",
                   class = "btn btn-primary",
+                  `data-bs-toggle`="modal",
+                  `data-bs-target`="#credits-modal",
                   "Credits"
                 )
               }
@@ -86,7 +136,60 @@ ui_csdta_header <- function(title,theory=TRUE,credits=TRUE,languages,csdta_link=
           )
         )
       )
+    ),
+    
+    # theory modal
+    div(
+      class = "modal fade",
+      id = "theory-modal",
+      tabindex = "-1",
+      `aria-labelledby` = "theoryModal",
+      `aria-hidden` = "true",
+      div(
+        class = "modal-dialog modal-fullscreen",
+        div(
+          class = "modal-content",
+          div(
+            class = "modal-header",
+            h1(
+              class = "modal-title fs-5",
+              id = "exampleModalLabel",
+              "Theory"
+            ),
+            button(
+              type = "button",
+              class = "btn-close",
+              `data-bs-dismiss` = "modal",
+              `aria-label` = "Close"
+            )
+          ),
+          div(
+            class = "modal-body",
+            modal_body_taglist
+          ),
+          div(
+            class = "modal-footer justify-content-between",
+            button(
+              type = "button",
+              class = "btn btn-secondary",
+              span(class = "glyphicon glyphicon-print me-2"),
+              "Export"
+            ),
+            button(
+              type = "button",
+              class = "btn btn-secondary",
+              `data-bs-dismiss` = "modal",
+              "Close"
+            )
+          )
+        )
+      )
     )
+
+  # tagsList END 
+  )
+
+  # withTags END
   )
 
 }
